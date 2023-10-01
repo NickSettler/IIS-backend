6c3f62d48192fc903dff768daddb4b00dee2502b
NickSettler <moiseevnikita14@gmail.com>
2023-10-01 23:57:23 +0200

    feat(auth): add auth constants and keys

---
+++ src/auth/constants.ts
@@ -0,0 +1,10 @@
+const defaultTokenExpiresIn = '15m';
+const defaultRefreshTokenExpiresIn = '30d';
+
+export const jwtConstants = {
+  tokenSecret: process.env.JWT_ACCESS_SECRET_KEY,
+  tokenExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN || defaultTokenExpiresIn,
+  refreshSecret: process.env.JWT_REFRESH_SECRET_KEY,
+  refreshExpiresIn:
+    process.env.JWT_REFRESH_EXPIRES_IN || defaultRefreshTokenExpiresIn,
+};
