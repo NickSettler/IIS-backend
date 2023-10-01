398f948ef7bc37347e4f2ff1a88678a4ff35750c
NickSettler <moiseevnikita14@gmail.com>
2023-10-01 23:56:27 +0200

    feat(users): add more user service methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from '../db/entities/user.entity';
-import { Repository } from 'typeorm';
+import { E_USER_ENTITY_KEYS, User } from '../db/entities/user.entity';
+import {
+  FindOneOptions,
+  FindOptionsWhere,
+  Repository,
+  UpdateResult,
+} from 'typeorm';
+import { CreateUserDto } from './users.dto';
+import { E_DB_ERROR_CODES } from '../db/constants';
 
 @Injectable()
 export class UsersService {
@@ -10,13 +21,47 @@ export class UsersService {
     private usersRepository: Repository<User>,
   ) {}
 
-  async findAll(): Promise<Array<User>> {
-    return this.usersRepository.find({
-      relations: ['roles', 'roles.permissions'],
+  public async findAll(): Promise<Array<User>> {
+    return this.usersRepository.find();
+  }
+
+  public async findOne(options: FindOneOptions<User>): Promise<User | null> {
+    return this.usersRepository.findOne(options);
+  }
+
+  public async findWithRefreshToken(
+    refreshToken: string,
+    criteria: FindOptionsWhere<User>,
+  ): Promise<User | false> {
+    const user = await this.findOne({
+      where: criteria,
+    });
+
+    if (!user) return false;
+
+    if (user[E_USER_ENTITY_KEYS.REFRESH_TOKEN] === refreshToken) return user;
+  }
+
+  public async create(createDto: CreateUserDto): Promise<User> {
+    const user = this.usersRepository.create(createDto);
+
+    return await this.usersRepository.save(user).catch((err: any) => {
+      if (err.code === E_DB_ERROR_CODES.UNIQUE_CONSTRAINT)
+        throw new ConflictException('User already exists');
+      else throw new InternalServerErrorException("Can't create user");
     });
   }
 
-  async delete(id: string): Promise<void> {
+  public async setRefreshToken(
+    id: string,
+    refreshToken: string,
+  ): Promise<void> {
+    await this.usersRepository.update(id, {
+      [E_USER_ENTITY_KEYS.REFRESH_TOKEN]: refreshToken,
+    });
+  }
+
+  public async delete(id: string): Promise<void> {
     await this.usersRepository.delete(id);
   }
 }
