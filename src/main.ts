5ea1c486562d90ab40624d50dfffabfa27c82257
NickSettler <moiseevnikita14@gmail.com>
2023-10-01 23:55:28 +0200

    feat: add using helmet and cookieParser

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -8,6 +10,10 @@ async function bootstrap() {
       credentials: true,
     },
   });
+
+  app.use(cookieParser());
+  app.use(helmet());
+
   await app.listen(3000);
 }
 bootstrap();
