84901e9eec07f82bb393e043d9704b81482c25c9
lenamarochkina <marochkina.lena@gmail.com>
2023-10-02 18:24:26 +0200

    feat: add triggers to check the schedule start and end dates

--- docker/postgres/migrations/006-Course.sql
+++ docker/postgres/migrations/006-Course.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS course;
 
 CREATE TABLE course
 (
-    abbr       VARCHAR(10) PRIMARY KEY,
-    garant_id  uuid         NOT NULL,
-    name       VARCHAR(255) NOT NULL UNIQUE,
-    credits    INT          NOT NULL CHECK (credits > 0 and credits < 20),
-    annotation VARCHAR(512),
-    CONSTRAINT fk_garant_id FOREIGN KEY (garant_id) REFERENCES users (id)
+    abbr         VARCHAR(10) PRIMARY KEY,
+    guarantor_id uuid         NOT NULL,
+    name         VARCHAR(255) NOT NULL UNIQUE,
+    credits      INT          NOT NULL CHECK (credits > 0 and credits < 20),
+    annotation   VARCHAR(512),
+    CONSTRAINT fk_guarantor_id FOREIGN KEY (guarantor_id) REFERENCES users (id)
 );
