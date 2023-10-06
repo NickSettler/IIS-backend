49ce0362cb929d209475e4d2e5c9ac86f52d9529
lenamarochkina <marochkina.lena@gmail.com>
2023-10-06 21:21:21 +0200

    fix: delete points from Student-Schedule table

--- docker/postgres/migrations/010-Student-Schedule.sql
+++ docker/postgres/migrations/010-Student-Schedule.sql
@@ -4,7 +4,6 @@ CREATE TABLE student_schedule
 (
     student_id  uuid NOT NULL,
     schedule_id uuid NOT NULL,
-    points      INT DEFAULT 0 CHECK (points >= 0 and points <= 100),
     CONSTRAINT fk_student_id FOREIGN KEY (student_id) REFERENCES users (id),
     CONSTRAINT fk_schedule_id FOREIGN KEY (schedule_id) REFERENCES schedule (id),
     PRIMARY KEY (student_id, schedule_id)
