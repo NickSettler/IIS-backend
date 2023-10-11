cca76f65afae523fee61dcc77dfa435199236f28
lenamarochkina <marochkina.lena@gmail.com>
2023-10-11 19:06:22 +0200

    feat: create module service for courses

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,8 +1,13 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Course } from '../db/entities/course.entity';
+import { Course, E_COURSE_ENTITY_KEYS } from '../db/entities/course.entity';
 import { FindOneOptions, Repository } from 'typeorm';
 import { isError } from '../utils/errors';
+import { UpdateCoursesDto } from './courses.dto';
 
 @Injectable()
 export class CoursesService {
@@ -18,6 +23,10 @@ export class CoursesService {
     return this.coursesRepository.find();
   }
 
+  /**
+   * Find one course using options
+   * @param options
+   */
   public async findOne(options: FindOneOptions<Course>) {
     return this.coursesRepository.findOne(options);
   }
@@ -32,5 +41,37 @@ export class CoursesService {
       throw err;
     });
     return this.coursesRepository.save(course);
+  }
+
+  /**
+   * Update a course
+   * @param abbr course abbreviation
+   * @param updateDto course data
+   */
+  public async update(
+    abbr: string,
+    updateDto: UpdateCoursesDto,
+  ): Promise<Course> {
+    const course = await this.coursesRepository.findOne({
+      where: { [E_COURSE_ENTITY_KEYS.ABBR]: abbr },
+    });
+    if (!course) throw new ConflictException('Course not found');
+
+    const updatedCourse = this.coursesRepository.merge(course, updateDto);
+
+    return await this.coursesRepository.save(updatedCourse);
+  }
+
+  /**
+   * Delete a course
+   * @param abbr course abbreviation
+   */
+  public async delete(abbr: string): Promise<void> {
+    const course = await this.coursesRepository.findOne({
+      where: { [E_COURSE_ENTITY_KEYS.ABBR]: abbr },
+    });
+    if (!course) throw new NotFoundException('User not found');
+
+    await this.coursesRepository.delete(abbr);
   }
 }
