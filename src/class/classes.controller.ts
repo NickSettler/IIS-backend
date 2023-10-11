fe51af33626c4885704d2ef86806437362b00429
lenamarochkina <marochkina.lena@gmail.com>
2023-10-11 20:18:42 +0200

    fix: add roles to controller

--- src/class/classes.controller.ts
+++ src/class/classes.controller.ts
@@ -15,6 +15,8 @@ import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
 import RolesGuard from '../common/guards/roles.guard';
 import { CreateClassDto, UpdateClassDto } from './classes.dto';
 import { ValidationPipe } from '../common/pipes/validation.pipe';
+import { Roles } from '../common/decorators/roles.decorator';
+import { E_ROLE } from '../db/entities/role.entity';
 
 @Controller('classes')
 export class ClassesController {
@@ -22,12 +24,20 @@ export class ClassesController {
 
   @Get()
   @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(
+    E_ROLE.ADMIN,
+    E_ROLE.TEACHER,
+    E_ROLE.STUDENT,
+    E_ROLE.GUARANTOR,
+    E_ROLE.SCHEDULER,
+  )
   public async getAll() {
     return this.classService.findAll();
   }
 
   @Get('/:abbr')
   @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(E_ROLE.ADMIN, E_ROLE.TEACHER, E_ROLE.GUARANTOR, E_ROLE.SCHEDULER)
   public async getOne(@Param('abbr') abbr: string) {
     return this.classService.findOne({
       where: { [E_CLASS_ENTITY_KEYS.ABBR]: abbr },
@@ -37,12 +47,14 @@ export class ClassesController {
   @Post()
   @UseGuards(JwtAuthGuard, RolesGuard)
   @UsePipes(ValidationPipe)
+  @Roles(E_ROLE.ADMIN)
   public async create(@Body() createDto: CreateClassDto) {
     return this.classService.create(createDto);
   }
 
   @Put('/:abbr')
   @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(E_ROLE.ADMIN)
   public async update(
     @Param('abbr') abbr: string,
     @Body() updateDto: UpdateClassDto,
@@ -51,6 +63,7 @@ export class ClassesController {
   }
 
   @Delete('/:abbr')
+  @Roles(E_ROLE.ADMIN)
   public async delete(@Param('abbr') abbr: string) {
     return this.classService.delete(abbr);
   }
