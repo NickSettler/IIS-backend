b9e5bbe3dc034524afc68fb07cd8403899ba6a6d
lenamarochkina <marochkina.lena@gmail.com>
2023-10-12 19:22:22 +0200

    feat: add error handling

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -2,7 +2,7 @@ import { E_POSTGRES_ERROR_CODES, E_SQLITE_ERROR_CODES } from '../db/constants';
 
 export const isError = (
   err: any,
-  code: keyof typeof E_POSTGRES_ERROR_CODES & keyof typeof E_SQLITE_ERROR_CODES,
+  code: keyof typeof E_POSTGRES_ERROR_CODES | keyof typeof E_SQLITE_ERROR_CODES,
 ): boolean => {
   if (!err) return false;
 
