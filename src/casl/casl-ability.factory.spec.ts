7aa91da45158595472ccea5cffff6bd4ec0c909c
NickSettler <moiseevnikita14@gmail.com>
2023-10-20 00:26:56 +0200

    test(auth): add abac testing

---
+++ src/casl/casl-ability.factory.spec.ts
@@ -0,0 +1,305 @@
+import { E_USER_ENTITY_KEYS, User } from '../db/entities/user.entity';
+import { Test } from '@nestjs/testing';
+import {
+  CaslAbilityFactory,
+  TAbility,
+  TSubjects,
+} from './casl-ability.factory';
+import { E_ACTION } from './actions';
+import { constant, reduce, times, values } from 'lodash';
+import { map } from 'lodash';
+import { Course } from '../db/entities/course.entity';
+import { Class } from '../db/entities/class.entity';
+import { Schedule } from '../db/entities/schedule.entity';
+import { CourseActivity } from '../db/entities/course_activity.entity';
+import { E_ROLE, E_ROLE_ENTITY_KEYS } from '../db/entities/role.entity';
+
+export const checkAbility = (
+  ability: TAbility,
+  actions: Array<E_ACTION>,
+  entity: TSubjects,
+  expected: Array<boolean>,
+): void => {
+  if (actions.length !== expected.length)
+    throw new Error('actions and expected arrays must have the same length');
+
+  actions.forEach((action, index) => {
+    it(`Can ${action} ${entity}`, () => {
+      expect(ability.can(action, entity)).toEqual(expected[index]);
+    });
+  });
+};
+
+export const generateExpected = (
+  actions: Array<E_ACTION>,
+  expected: Array<boolean>,
+): Partial<Record<E_ACTION, boolean>> => {
+  if (actions.length !== expected.length)
+    throw new Error('actions and expected arrays must have the same length');
+
+  return reduce(
+    actions,
+    (acc, action, index) => ({
+      ...acc,
+      [action]: expected[index],
+    }),
+    {},
+  );
+};
+
+describe('CaslAbilityFactory test', () => {
+  let abilityFactory: CaslAbilityFactory;
+  let user: User;
+  let ability: TAbility;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [CaslAbilityFactory],
+    }).compile();
+
+    abilityFactory = await module.resolve(CaslAbilityFactory);
+  });
+
+  describe('Guest user', () => {
+    beforeEach(() => {
+      ability = abilityFactory.createForUser(user);
+    });
+
+    describe('Users', () => {
+      const expected = generateExpected(
+        values(E_ACTION),
+        times(4, constant(false)),
+      );
+
+      map(expected, (value, action: E_ACTION) => {
+        it(`Can ${action} users`, () => {
+          expect(ability.can(action, User)).toEqual(value);
+        });
+      });
+    });
+
+    describe('Class', () => {
+      const expected = generateExpected(
+        values(E_ACTION),
+        times(4, constant(false)),
+      );
+
+      map(expected, (value, action: E_ACTION) => {
+        it(`Can ${action} classes`, () => {
+          expect(ability.can(action, Class)).toEqual(value);
+        });
+      });
+    });
+
+    describe('Course', () => {
+      const expected = generateExpected(values(E_ACTION), [
+        false,
+        true,
+        false,
+        false,
+      ]);
+
+      map(expected, (value, action: E_ACTION) => {
+        it(`Can ${action} courses`, () => {
+          expect(ability.can(action, Course)).toEqual(value);
+        });
+      });
+    });
+
+    describe('Course activity', () => {
+      const expected = generateExpected(
+        values(E_ACTION),
+        times(4, constant(false)),
+      );
+
+      map(expected, (value, action: E_ACTION) => {
+        it(`Can ${action} course activities`, () => {
+          expect(ability.can(action, CourseActivity)).toEqual(value);
+        });
+      });
+    });
+
+    describe('Schedule', () => {
+      const expected = generateExpected(
+        values(E_ACTION),
+        times(4, constant(false)),
+      );
+
+      map(expected, (value, action: E_ACTION) => {
+        it(`Can ${action} schedule items`, () => {
+          expect(ability.can(action, Schedule)).toEqual(value);
+        });
+      });
+    });
+  });
+
+  describe('Student', () => {
+    beforeEach(() => {
+      user = new User();
+      user[E_USER_ENTITY_KEYS.ROLES] = [
+        {
+          [E_ROLE_ENTITY_KEYS.NAME]: E_ROLE.STUDENT,
+          [E_ROLE_ENTITY_KEYS.PERMISSIONS]: [],
+        },
+      ];
+      ability = abilityFactory.createForUser(user);
+    });
+
+    describe('Users', () => {
+      const expected = generateExpected(
+        values(E_ACTION),
+        times(4, constant(false)),
+      );
+
+      map(expected, (value, action: E_ACTION) => {
+        it(`Can ${action} users`, () => {
+          expect(ability.can(action, User)).toEqual(value);
+        });
+      });
+    });
+
+    describe('Class', () => {
+      const expected = generateExpected(values(E_ACTION), [
+        false,
+        true,
+        false,
+        false,
+      ]);
+
+      map(expected, (value, action: E_ACTION) => {
+        it(`Can ${action} classes`, () => {
+          expect(ability.can(action, Class)).toEqual(value);
+        });
+      });
+    });
+
+    describe('Course', () => {
+      const expected = generateExpected(values(E_ACTION), [
+        false,
+        true,
+        false,
+        false,
+      ]);
+
+      map(expected, (value, action: E_ACTION) => {
+        it(`Can ${action} courses`, () => {
+          expect(ability.can(action, Course)).toEqual(value);
+        });
+      });
+    });
+
+    describe('Course activity', () => {
+      const expected = generateExpected(values(E_ACTION), [
+        false,
+        true,
+        false,
+        false,
+      ]);
+
+      map(expected, (value, action: E_ACTION) => {
+        it(`Can ${action} course activities`, () => {
+          expect(ability.can(action, CourseActivity)).toEqual(value);
+        });
+      });
+    });
+
+    describe('Schedule', () => {
+      const expected = generateExpected(values(E_ACTION), [
+        false,
+        true,
+        false,
+        false,
+      ]);
+
+      map(expected, (value, action: E_ACTION) => {
+        it(`Can ${action} schedule items`, () => {
+          expect(ability.can(action, Schedule)).toEqual(value);
+        });
+      });
+    });
+  });
+
+  describe('Scheduler', () => {
+    beforeEach(() => {
+      user = new User();
+      user[E_USER_ENTITY_KEYS.ROLES] = [
+        {
+          [E_ROLE_ENTITY_KEYS.NAME]: E_ROLE.SCHEDULER,
+          [E_ROLE_ENTITY_KEYS.PERMISSIONS]: [],
+        },
+      ];
+      ability = abilityFactory.createForUser(user);
+    });
+
+    describe('Users', () => {
+      const expected = generateExpected(
+        values(E_ACTION),
+        times(4, constant(false)),
+      );
+
+      map(expected, (value, action: E_ACTION) => {
+        it(`Can ${action} users`, () => {
+          expect(ability.can(action, User)).toEqual(value);
+        });
+      });
+    });
+
+    describe('Class', () => {
+      const expected = generateExpected(values(E_ACTION), [
+        false,
+        true,
+        false,
+        false,
+      ]);
+
+      map(expected, (value, action: E_ACTION) => {
+        it(`Can ${action} classes`, () => {
+          expect(ability.can(action, Class)).toEqual(value);
+        });
+      });
+    });
+
+    describe('Course', () => {
+      const expected = generateExpected(values(E_ACTION), [
+        false,
+        true,
+        false,
+        false,
+      ]);
+
+      map(expected, (value, action: E_ACTION) => {
+        it(`Can ${action} courses`, () => {
+          expect(ability.can(action, Course)).toEqual(value);
+        });
+      });
+    });
+
+    describe('Course activity', () => {
+      const expected = generateExpected(values(E_ACTION), [
+        false,
+        true,
+        false,
+        false,
+      ]);
+
+      map(expected, (value, action: E_ACTION) => {
+        it(`Can ${action} course activities`, () => {
+          expect(ability.can(action, CourseActivity)).toEqual(value);
+        });
+      });
+    });
+
+    describe('Schedule', () => {
+      const expected = generateExpected(
+        values(E_ACTION),
+        times(4, constant(true)),
+      );
+
+      map(expected, (value, action: E_ACTION) => {
+        it(`Can ${action} schedule items`, () => {
+          expect(ability.can(action, Schedule)).toEqual(value);
+        });
+      });
+    });
+  });
+});
