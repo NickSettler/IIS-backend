cbd6dc4e49bffcaedcd32d882e22d14d031fc535
NickSettler <moiseevnikita14@gmail.com>
2023-10-20 01:55:29 +0200

    fix(db): move teacher_id to schedule table

--- docker/postgres/migrations/007-Course-Activity.sql
+++ docker/postgres/migrations/007-Course-Activity.sql
@@ -8,9 +8,7 @@ CREATE TYPE course_forms AS ENUM ('LECTURE',
 CREATE TABLE course_activity
 (
     id          uuid DEFAULT gen_random_uuid() PRIMARY KEY,
-    teacher_id  uuid         NOT NULL,
     course_abbr VARCHAR(10)  NOT NULL,
     form        course_forms NOT NULL,
-    CONSTRAINT fk_teacher_id FOREIGN KEY (teacher_id) REFERENCES users (id),
     CONSTRAINT fk_course_abbr FOREIGN KEY (course_abbr) REFERENCES courses (abbr)
 );
--- docker/postgres/migrations/010-Schedule.sql
+++ docker/postgres/migrations/010-Schedule.sql
@@ -8,9 +8,11 @@ CREATE TABLE schedule
 (
     id                 uuid DEFAULT gen_random_uuid() PRIMARY KEY,
     course_activity_id uuid        NOT NULL,
+    teacher_id         uuid        NOT NULL,
     class_abbr         VARCHAR(10) NOT NULL,
     start_time         TIMESTAMP   NOT NULL,
     end_time           TIMESTAMP   NOT NULL CHECK (end_time > start_time),
+    CONSTRAINT fk_teacher_id FOREIGN KEY (teacher_id) REFERENCES users (id),
     CONSTRAINT fk_course_activity_id FOREIGN KEY (course_activity_id) REFERENCES course_activity (id),
     CONSTRAINT fk_class_abbr FOREIGN KEY (class_abbr) REFERENCES classes (abbr)
 );
