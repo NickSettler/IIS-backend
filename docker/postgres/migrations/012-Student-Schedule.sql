2383d13fc077bfcad20061aa823ef0512c20ed7d
NickSettler <moiseevnikita14@gmail.com>
2023-10-22 14:24:11 +0200

    feat(db): add checking if user is a student for student schedule table

--- docker/postgres/migrations/012-Student-Schedule.sql
+++ docker/postgres/migrations/012-Student-Schedule.sql
@@ -1,3 +1,5 @@
+DROP TRIGGER IF EXISTS check_user_is_student ON student_schedule;
+DROP FUNCTION IF EXISTS check_user_is_student();
 DROP TABLE IF EXISTS student_schedule;
 
 CREATE TABLE student_schedule
@@ -8,3 +10,35 @@ CREATE TABLE student_schedule
     CONSTRAINT fk_schedule_id FOREIGN KEY (schedule_id) REFERENCES schedule (id) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY (student_id, schedule_id)
 );
+
+CREATE FUNCTION check_user_is_student()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    user_username VARCHAR;
+BEGIN
+    PERFORM *
+    FROM user_roles
+    WHERE user_id = NEW.student_id
+      AND role_name = 'STUDENT';
+
+    IF NOT FOUND THEN
+        SELECT username
+        INTO user_username
+        FROM users
+        WHERE id = NEW.student_id;
+
+        RAISE EXCEPTION 'User % is not a student', user_username USING ERRCODE = 'C0063';
+    END IF;
+
+    RETURN NEW;
+END
+$$;
+
+CREATE TRIGGER check_user_is_student
+    BEFORE INSERT OR UPDATE
+    ON student_schedule
+    FOR EACH ROW
+EXECUTE PROCEDURE check_user_is_student();
